4bcf531e860ff3bd3254ec39354229a0b7fd6272
rohitporoli <rohitp4all@gmail.com>
2017-07-11 15:11:49 +0530

    mido: Add device maintainer info
    *showcase myself
    *gain popularity
    *show-off

--- lineage.mk
+++ lineage.mk
@@ -27,3 +27,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="xiaomi/mido/mido:6.0.1/MMB29M/V8.2.4.0.MCFMIDL:user/release-keys" \
     PRIVATE_BUILD_DESC="mido-user 6.0.1 MMB29M V8.2.4.0.MCFMIDL release-keys"
+
+# Citrus-CAF Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+         DEVICE_MAINTAINERS="Rohit (rohitporoli)"
